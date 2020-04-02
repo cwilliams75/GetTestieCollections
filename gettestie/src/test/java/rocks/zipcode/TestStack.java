a35be0282528b39cb67c64fa357015e2bdfc3363
cwilliams <coreywllms9@gmail.com>
2020-04-02 19:06:25 -0400

    next steps

--- gettestie/src/test/java/rocks/zipcode/TestStack.java
+++ gettestie/src/test/java/rocks/zipcode/TestStack.java
@@ -1,12 +1,22 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 import java.util.Stack;
 
 public class TestStack {
 
+    Stack<String> stack;
+
     @org.junit.Before
     public void setUp() throws Exception {
+        stack = new Stack<>();
+        stack.push("First In");
+        stack.push("Second In");
+        stack.push("Third In");
+        stack.push("Fourth In");
     }
 
     @org.junit.After
@@ -20,5 +30,27 @@ public class TestStack {
         assertEquals(false, stack.isEmpty()); // false
     }
 
+
     // Make a bigger test exercising more Stack methods.....
+    //push,pop,peek,search,empty
+    @Test
+    public void popTest(){
+        Assert.assertEquals("Fourth In",stack.pop());
+    }
+
+    @Test
+    public void peekTest(){
+        Assert.assertEquals("Fourth In",stack.peek());
+    }
+
+    @Test
+    public void searchTest(){
+        Assert.assertEquals(4,stack.search("First In"));
+    }
+
+    @Test
+    public void emptyTest(){
+        stack.clear();
+        Assert.assertTrue(stack.isEmpty());
+    }
 }
---
+++ gettestie/src/test/java/rocks/zipcode/TestTreeSet.java
@@ -0,0 +1,97 @@
+package rocks.zipcode;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.TreeSet;
+
+public class TestTreeSet {
+
+    TreeSet<String> treeSet;
+
+    @Before
+    public void setUp() throws Exception{
+        treeSet = new TreeSet<>();
+        treeSet.add("One");
+
+    }
+
+    @After
+    public void tearDown() throws Exception{
+
+    }
+
+    //size,last,poll
+
+    @Test
+    public void sizeTest(){
+        Assert.assertEquals(1, treeSet.size());
+    }
+
+    @Test
+    public void lastTest(){
+        for (int i = 0; i < 5; i++) {
+            treeSet.add("another entry " + i);
+        }
+        Assert.assertEquals("another entry 4", treeSet.last());
+    }
+
+    @Test
+    public void pollTest(){
+        treeSet.clear();
+        Assert.assertEquals(null, treeSet.pollFirst());
+    }
+
+    @Test
+    public void lastHighestElmTest(){
+        treeSet.add("Two");
+        treeSet.add("Three");
+        treeSet.add("Four");
+        treeSet.add("Five");
+        treeSet.add("Six");
+        String lastElm = treeSet.last();
+        String expected = "Two";
+        Assert.assertEquals(expected,lastElm);
+    }
+
+    @Test
+    public void iteratorTest(){
+        treeSet.add("Two");
+        treeSet.add("Three");
+        treeSet.add("Four");
+        treeSet.add("Five");
+        treeSet.add("Six");
+        Iterator iterator = treeSet.iterator();
+        int expected = 6;
+        int actual = 0;
+        for (Iterator it = iterator; it.hasNext(); ) {
+            it.next();
+            actual = actual + 1;
+        }
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void higherTest(){
+        treeSet.add("Two");
+        treeSet.add("Three");
+        treeSet.add("Four");
+        treeSet.add("Five");
+        treeSet.add("Six");
+
+        Assert.assertEquals(null,treeSet.higher("Two"));
+    }
+
+    @Test
+    public void lowerTest(){
+        treeSet.add("Two");
+        treeSet.add("Three");
+        treeSet.add("Four");
+        treeSet.add("Five");
+        treeSet.add("Six");
+        Assert.assertEquals("Three",treeSet.lower("Two"));
+    }
+}
